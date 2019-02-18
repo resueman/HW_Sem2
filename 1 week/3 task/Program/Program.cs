73bc5e038bd8e0ca19a7cc937086553d739f6b66
Ulyana <u.katrichko@yandex.ru>
2019-02-18 11:57:32 +0300

    3 task solution

---
+++ 1 week/3 task/Program/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Program
+{
+    class Program
+    {
+        static int[] CreateArray(int size)
+        {
+            Random random = new Random();
+            int[] newArray = new int[size];
+            for (int i = 0; i < size; ++i)
+            {
+                newArray[i] = random.Next(-15, 15);
+            }
+            return newArray;
+        }
+
+        static void PrintArray(int[] array)
+        {
+            foreach(int element in array)
+            {
+                Console.Write($"{element} ");
+            }
+            Console.WriteLine();
+        }
+
+        static void Main()
+        {
+            Console.Write("Enter the number of elements:  ");
+            int number = int.Parse(Console.ReadLine());
+            int[] array = CreateArray(number);
+            Console.WriteLine("Original Array:");
+            PrintArray(array);
+            Console.WriteLine("Result array:");
+            Sort.QuickSort(array, 0, number - 1);
+            PrintArray(array);
+        }
+    }
+}
+
---
+++ 1 week/3 task/Program/Sort.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Program
+{
+    class Sort
+    {
+        static int Partition(int[] array, int left, int right)
+        {
+            int first = array[left];
+            int i = left + 1;
+            for (int j = left + 1; j < right + 1; ++j)
+            {
+                if (array[j] <= first)
+                {
+                    Swap(ref array[j], ref array[i]);
+                    i++;
+                }
+            }
+            Swap(ref array[left], ref array[i - 1]);
+            return i - 1;
+        }
+
+        static void Swap(ref int a, ref int b)
+        {
+            int buffer = a;
+            a = b;
+            b = buffer;
+        }
+
+        public static void QuickSort(int[] array, int left, int right)
+        {
+            if (right <= left)
+            {
+                return;
+            }
+            int pivot = Partition(array, left, right);
+            QuickSort(array, left, pivot - 1);
+            QuickSort(array, pivot + 1, right);
+        }
+    }
+}
