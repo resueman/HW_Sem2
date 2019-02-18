cce332fe3f9718310a20ff2a34efa6e456b44510
Ulyana <u.katrichko@yandex.ru>
2019-02-18 14:40:52 +0300

    some experiments with classes, constructors, accessors

---
+++ 1 week/4 task/Program/Matrix.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace Program
+{
+    class Matrix
+    {
+        public int size;
+        public int[,] matrix;
+
+        public Matrix(int size)
+        {
+            this.size = size;
+            matrix = new int [this.size, this.size];
+        }
+
+        public int this[int i, int j]
+        {
+            get
+            {
+                return matrix[i, j];
+            }
+        }
+
+        public int[,] Initialization()
+        {
+            Random random = new Random();
+            for (int i = 0; i < size; ++i)
+            {
+                for (int j = 0; j < size; ++j)
+                {
+                    matrix[i, j] = random.Next(1, 9);
+                }
+            }
+            return matrix;
+        }
+
+        public void PrintMatrix()
+        {
+            for (int i = 0; i < size; ++i)
+            {
+                for (int j = 0; j < size; ++j)
+                {
+                    Console.Write("{0,5}", matrix[i, j]);
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+        }
+    }
+}
--- 1 week/4 task/Program/Program.cs
+++ 1 week/4 task/Program/Program.cs
@@ -13,18 +13,21 @@ namespace Program
                 Console.WriteLine("Size should be an odd number");
                 return;
             }
-            int [,] matrix = CreateMatrix(size);
-            PrintMatrix(matrix);
+
+            Matrix matrix = new Matrix(size);
+            matrix.Initialization();
+            matrix.PrintMatrix();
             SpiralBypass(matrix);
         }
 
-        static void SpiralBypass(int[,] matrix)
+        static void SpiralBypass(Matrix matrix)
         {
             int delta = 1;
             int direction = 1;
-            int size = matrix.GetLength(0);
+            int size = matrix.size;
             int i = size / 2;
             int j = size / 2;
+
             Console.Write($"{matrix[i, j]}  ");
 
             while (delta < size)
@@ -50,33 +53,6 @@ namespace Program
                 i -= direction;
                 Console.Write($"{matrix[i, j]}  ");//last up
             }
-        }
-
-        static int[,] CreateMatrix(int size)
-        {
-            int[,] matrix = new int[size, size];
-            Random random = new Random();
-            for (int i = 0; i < matrix.GetLength(0); ++i)
-            {
-                for (int j = 0; j < matrix.GetLength(0); ++j)
-                {
-                    matrix[i, j] = random.Next(0, 9);
-                }
-            }
-            return matrix;
-        }
-
-        static void PrintMatrix(int[,] matrix)
-        {
-            for (int i = 0; i < matrix.GetLength(0); ++i)
-            {
-                for (int j = 0; j < matrix.GetLength(0); ++j)
-                {
-                    Console.Write("{0,5}", matrix[i, j]);
-                }
-                Console.WriteLine();
-            }
-            Console.WriteLine();
         }
     }
 }
