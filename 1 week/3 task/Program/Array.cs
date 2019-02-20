74f6db774cd23aa7d1a6e2e39c66de8cf5971505
Ulyana <u.katrichko@yandex.ru>
2019-02-20 18:14:20 +0300

    made program more modular? add class Array

---
+++ 1 week/3 task/Program/Array.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Program
+{
+    class Array
+    {
+        public static int[] CreateArray(int size)
+        {
+            int[] newArray = new int[size];
+            Random random = new Random();
+            for (int i = 0; i < size; ++i)
+            {
+                newArray[i] = random.Next(-15, 15);
+            }
+            return newArray;
+        }
+
+        public static void PrintArray(int[] array)
+        {
+            foreach (int element in array)
+            {
+                Console.Write($"{element} ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
--- 1 week/3 task/Program/Program.cs
+++ 1 week/3 task/Program/Program.cs
@@ -4,37 +4,16 @@ namespace Program
 {
     class Program
     {
-        static int[] CreateArray(int size)
-        {
-            Random random = new Random();
-            int[] newArray = new int[size];
-            for (int i = 0; i < size; ++i)
-            {
-                newArray[i] = random.Next(-15, 15);
-            }
-            return newArray;
-        }
-
-        static void PrintArray(int[] array)
-        {
-            foreach(int element in array)
-            {
-                Console.Write($"{element} ");
-            }
-            Console.WriteLine();
-        }
-
         static void Main()
         {
             Console.Write("Enter the number of elements:  ");
             int number = int.Parse(Console.ReadLine());
-            int[] array = CreateArray(number);
+            int[] array = Array.CreateArray(number);
             Console.WriteLine("Original Array:");
-            PrintArray(array);
+            Array.PrintArray(array);
             Console.WriteLine("Result array:");
             Sort.QuickSort(array, 0, number - 1);
-            PrintArray(array);
+            Array.PrintArray(array);
         }
     }
 }
-
