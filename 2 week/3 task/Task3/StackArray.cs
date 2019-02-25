d7348c3dc0920e155ee2ae044b450139c57c8ea2
Ulyana <u.katrichko@yandex.ru>
2019-02-25 12:57:03 +0300

    written stack-list

---
+++ 2 week/3 task/Task3/StackArray.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Task3
+{
+    class StackArray<T>
+    {
+        private T[] stack;
+        private int head = -1;
+
+        public StackArray(int size)
+        {
+            var stack = new int[size];
+        }
+
+        public bool IsEmpty()
+            => head == -1;
+
+        public T Top()
+        => stack[head - 1];
+
+        public T Pop()
+        {
+            if (IsEmpty())
+            {
+                throw new Exception("Stack is empty!");
+            }
+            --head;
+            return stack[head];
+        }
+
+        public void Push(T value)
+        {
+            stack[head] = value;
+            ++head;
+        }
+    }
+}
---
+++ 2 week/3 task/Task3/StackList.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Task3
+{
+    class StackList<T> : IStack<T>
+    {
+        Node head = null;
+
+        class Node
+        {
+            public T Value { get; set; }
+            public Node Next { get; set; } = null;
+
+            public Node(T value)
+            {
+                Value = value;
+            }
+        }
+
+        public bool IsEmpty()
+            => head == null;
+       
+        public T Top()
+            => head.Value;
+
+        public void Push(T value)
+        {
+            var newNode = new Node(value);
+            newNode.Next = head;
+            head = newNode;
+        }
+
+        public T Pop()
+        {
+            if (IsEmpty())
+            {
+                throw new Exception("Stack is empty!");
+            }
+            T topValue = head.Value;
+            head = head.Next;
+            return topValue;
+        }
+    }
+}
