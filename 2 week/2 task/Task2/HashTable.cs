eccc007c9124b14deeb06d9044213550c5ca7dd1
Ulyana <u.katrichko@yandex.ru>
2019-02-28 14:46:55 +0300

    wrote some functions

---
+++ 2 week/2 task/Task2/HashTable.cs
@@ -0,0 +1,75 @@
+﻿namespace Task2
+{
+    class HashTable<T>//:IHashTable<T>
+    {
+        List<T>[] buckets;
+
+        public HashTable(int size)
+        {
+            Resize(size);
+        }
+
+        private void Resize(int size)
+        {
+            var newBuckets = new List<T>[size];
+            buckets = newBuckets;
+        }
+
+        public void AddToHashTable(T input)
+        {
+            int hash = input.GetHashCode() % buckets.Length;
+            buckets[hash].AddNode(input, 1);
+        }
+
+        public void PrintHashTable()
+        {
+            for (int i = 0; i < buckets.Length; ++i)
+            {
+                buckets[i].PrintList();
+            }
+        }
+
+        public double LoadFactor()
+        {
+            int filled = 0;
+            for (int i = 0; i < buckets.Length; ++i)
+            {
+                if (buckets[i] != null)
+                {
+                    ++filled;
+                }
+            }
+            return 1.0 * filled / buckets.Length;
+        }
+
+        int AverageListLength()
+        {
+            int lenghtSum = 0;
+            for (int i = 0; i < buckets.Length; ++i)
+            {
+                if (buckets[i] != null)
+                {
+                    lenghtSum += buckets[i].GetLengthOfList();
+                }
+            }
+            return lenghtSum / buckets.Length;
+        }
+
+        int MaxListLength()
+        {
+            int maxListLenght = 0;
+            for (int i = 0; i < buckets.Length; ++i)
+            {
+                if (buckets[i] != null)
+                {
+                    int currentLenght = buckets[i].GetLengthOfList();
+                    if (currentLenght > maxListLenght)
+                    {
+                        maxListLenght = currentLenght;
+                    }
+                }
+            }
+            return maxListLenght;
+        }
+    }
+}
---
+++ 2 week/2 task/Task2/List.cs
@@ -0,0 +1,135 @@
+﻿using System;
+
+namespace Task2
+{
+    class List<T> : IList<T>
+    {
+        private int length = 0;
+        private Node head = null;
+
+        private class Node
+        {
+            public T Value { get; set; }
+            public Node Next { get; set; } = null;
+
+            public Node(T value)
+            {
+                Value = value;
+            }
+        }
+
+        public bool IsEmpty()
+        => length == 0;
+
+        public int GetLengthOfList()
+        {
+            return length;
+        }
+
+        private Node GetPreviousNodeByPosition(int position)
+        {
+            Node current = head;
+            for (int i = 0; i < position - 2; ++i)
+            {
+                current = current.Next;
+            }
+            return current;
+        }
+
+        private void InsertToHead(Node newNode)
+        {
+            newNode.Next = head;
+            head = newNode;
+        }
+
+        private void Insert(Node newNode, int position)
+        {
+            var previous = GetPreviousNodeByPosition(position);
+            newNode.Next = previous.Next;
+            previous.Next = newNode;
+        }
+
+        public void AddNode(T value, int position)
+        {
+            if (position > length + 1 && position != 0 || position < 1)
+            {
+                return;
+            }
+            Node newNode = new Node(value);
+            if (position == 1)
+            {
+                InsertToHead(newNode);
+            }
+            else
+            {
+                Insert(newNode, position);
+            }
+            ++length;
+        }
+
+        private bool IsCorrectPosition(int position)
+        {
+            if (position > length || position < 1)
+            {
+                return false;
+            }
+            return true;
+        }
+
+        public T GetValue(int position)
+        {
+            if (IsCorrectPosition(position))
+            {
+                //return -666;
+            }
+            return GetPreviousNodeByPosition(position + 1).Value;
+        }
+
+        public void SetValue(T value, int position)
+        {
+            if (!IsCorrectPosition(position))
+            {
+                return;
+            }
+            GetPreviousNodeByPosition(position + 1).Value = value;
+        }
+
+        public void DeleteNode(int position)
+        {
+            if (!IsCorrectPosition(position))
+            {
+                return;
+            }
+            if (position == 1)
+            {
+                head = head.Next;
+            }
+            else
+            {
+                var previousNode = GetPreviousNodeByPosition(position);
+                previousNode.Next = previousNode.Next.Next;
+            }
+            --length;
+        }
+
+        public void PrintList()
+        {
+            Node current = head;
+            for (int i = 0; i < length; ++i)
+            {
+                Console.Write($"{current.Value.ToString()}  ");
+                current = current.Next;
+            }
+        }
+
+        public void Clear()
+        {
+            Node current = head;
+            while (length > 0)
+            {
+                DeleteNode(1);
+                current = current.Next;
+            }
+        }
+    }
+}
