8c2607628b44a10491223900557dbc229700e722
Ulyana <u.katrichko@yandex.ru>
2019-03-11 15:12:10 +0300

    created project

---
+++ 3 week/1 task/Task3/IStack.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Task3
+{
+    interface IStack
+    {
+        int Top();
+        int Pop(ref bool result);
+        bool IsEmpty();
+        void Push(int value);
+    }
+}
---
+++ 3 week/1 task/Task3/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Task3
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Enter the line separating the numbers and signs of the operations by spaces");
+            Console.Write("Expression:  ");
+            string expression = Console.ReadLine();
+            bool isCorrectInput = true;
+
+            var stackArray = new StackArray();
+            var stackList = new StackList();
+            var caluclatorArray = new Calculator(stackArray);
+            var calculatorList = new Calculator(stackList);
+
+            int resultArray = caluclatorArray.Calculation(expression, ref isCorrectInput);
+            int resultList = calculatorList.Calculation(expression, ref isCorrectInput);            
+
+            if (!isCorrectInput)
+            {
+                Console.WriteLine("Incorrect input");
+                return;
+            }
+            Console.WriteLine(resultArray);
+            Console.WriteLine(resultList);
+        }
+    }
+}
