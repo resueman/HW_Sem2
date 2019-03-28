fac394475b7c9fc9b967665ecb5ca4eae3496a2a
Ulyana <u.katrichko@yandex.ru>
2019-03-28 22:32:23 +0300

    rename

---
+++ 2week/2task/2task/Task2/IList.cs
@@ -0,0 +1,13 @@
+﻿namespace Task2
+{
+    interface IList<T>
+    {
+        void AddNode(T value, int position);
+        void DeleteNode(int position);
+        T GetValue(int position);
+        void SetValue(T value, int position);
+        int GetLengthOfList();
+        bool IsEmpty();
+        void PrintList();
+    }
+}
