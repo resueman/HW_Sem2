6e81c1131e05fe1676fc3c3c26ef22ef8131a6e8
Ulyana <u.katrichko@yandex.ru>
2019-03-31 22:19:34 +0300

    add solution

---
+++ 2week/2task/Task2/IncorrectPositionException.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+
+namespace Task2
+{
+    [Serializable()]
+    public class IncorrectPositionException : Exception
+    {
+        public IncorrectPositionException()
+        {
+        }
+        public IncorrectPositionException(string message)
+            : base(message)
+        {
+        }
+        public IncorrectPositionException(string message, Exception inner)
+            : base(message, inner)
+        {
+        }
+        protected IncorrectPositionException(System.Runtime.Serialization.SerializationInfo info,
+        System.Runtime.Serialization.StreamingContext context) : base(info, context) { }
+    }
+    public class MyException : Exception
+    {
+        public MyException()
+        {
+        }
+        public MyException(string message)
+            : base(message)
+        {
+        }
+        public MyException(string message, Exception inner)
+            : base(message, inner)
+        {
+        }
+        protected MyException(System.Runtime.Serialization.SerializationInfo info,
+        System.Runtime.Serialization.StreamingContext context) : base(info, context) { }
+    }
+}
---
+++ 2week/2task/Task2/List.cs
@@ -0,0 +1,164 @@
+﻿using System;
+
+namespace Task2
+{
+    class List<T> : IList<T>
+    {
+        private int length = 0;
+        private Node head = null;
+
+        private class Node
+        {
+            public T Value { get; set; }
+            public Node Next { get; set; } = null;
+
+            public Node(T value)
+            {
+                Value = value;
+            }
+        }
+
+        public bool IsEmpty()
+        => length == 0;
+
+        public int GetLengthOfList()
+        {
+            return length;
+        }
+
+        private Node GetPreviousNodeByPosition(int position)
+        {
+            Node current = head;
+            for (int i = 0; i < position - 2; ++i)
+            {
+                current = current.Next;
+            }
+            return current;
+        }
+
+        private void InsertToHead(Node newNode)
+        {
+            newNode.Next = head;
+            head = newNode;
+        }
+
+        private void Insert(Node newNode, int position)
+        {
+            var previous = GetPreviousNodeByPosition(position);
+            newNode.Next = previous.Next;
+            previous.Next = newNode;
+        }
+
+        public void AddNode(T value, int position)
+        {
+            if (position > length + 1 && position != 0 || position < 1)
+            {
+                throw new IncorrectPositionException("Can't add node, incorrect position");
+            }
+            Node newNode = new Node(value);
+            if (position == 1)
+            {
+                InsertToHead(newNode);
+            }
+            else
+            {
+                Insert(newNode, position);
+            }
+            ++length;
+        }
+
+        private void IsCorrectPosition(int position)
+        {
+            if (position > length || position < 1)
+            {
+                throw new IncorrectPositionException("Incorrect position");
+            }
+        }
+
+        public T GetValue(int position)
+        {
+            try
+            {
+                IsCorrectPosition(position);
+                return GetPreviousNodeByPosition(position + 1).Value;
+            }
+            catch(IncorrectPositionException exception)
+            {
+                throw new MyException("Can't get value", exception);
+            }
+        }
+
+        public void SetValue(T value, int position)
+        {
+            try
+            {
+                IsCorrectPosition(position);
+                GetPreviousNodeByPosition(position + 1).Value = value;
+            }
+            catch (IncorrectPositionException exception)
+            {
+                throw new MyException("Can't set value", exception);
+            }
+        }
+
+        public void DeleteNode(int position)
+        {
+            try
+            {
+                IsCorrectPosition(position);
+                if (position == 1)
+                {
+                    head = head.Next;
+                }
+                else
+                {
+                    var previousNode = GetPreviousNodeByPosition(position);
+                    previousNode.Next = previousNode.Next.Next;
+                }
+                --length;
+            }
+            catch (IncorrectPositionException exception)
+            {
+                throw new MyException("Can't delete node", exception);
+            }
+        }
+
+        public void PrintList()
+        {
+            Node current = head;
+            for (int i = 0; i < length; ++i)
+            {
+                Console.Write($"{current.Value.ToString()}  ");
+                current = current.Next;
+            }
+        }
+
+        public void Clear()
+        {
+            Node current = head;
+            while (length > 0)
+            {
+                DeleteNode(1);
+                current = current.Next;
+            }
+        }
+
+        public int FindPosition(T value)
+        {
+            if(head == null)
+            {
+                return -1;
+            }
+            var current = head;
+            for(int i = 0; i < length; ++i)
+            {
+                if(current.Value.Equals(value))
+                {
+                    return i + 1; 
+                }
+                current = current.Next;
+            }
+            return -1;
+        }
+    }
+}
