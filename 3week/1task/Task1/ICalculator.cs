bc50e8e4f425c967863d159bb8f262e672fa606c
Ulyana <u.katrichko@yandex.ru>
2019-04-11 18:45:34 +0300

    added comments

--- 3week/1task/Task1/ICalculator.cs
+++ 3week/1task/Task1/ICalculator.cs
@@ -2,6 +2,11 @@
 {
     public interface ICalculator
     {
+        /// <summary>
+        /// Calculate the value of expression
+        /// </summary>
+        /// <param name="expression">Expression in postfix form</param>
+        /// <returns>Expression value</returns>
         int Calculation(string expression);
     }
 }
--- 3week/1task/Task1/IStack.cs
+++ 3week/1task/Task1/IStack.cs
@@ -1,13 +1,33 @@
 ﻿namespace Task1
 {
+    /// <summary>
+    /// Stack interface
+    /// </summary>
+    /// <typeparam name="T">Type of stack elements</typeparam>
     public interface IStack<T>
     {
+        /// <summary>
+        /// Returns last added element
+        /// </summary>
+        /// <returns>Element from top of stack</returns>
         T Top();
 
+        /// <summary>
+        /// Deletes element from top of stack
+        /// </summary>
+        /// <returns>Value of popped element</returns>
         T Pop();
 
+        /// <summary>
+        /// Checks if stack is empty
+        /// </summary>
+        /// <returns>True if stack is empty, false if stack isn't empty </returns>
         bool IsEmpty();
 
+        /// <summary>
+        /// Add element to top of stack
+        /// </summary>
+        /// <param name="value">Value to add</param>
         void Push(T value);
     }
 }
