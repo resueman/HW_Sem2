26e7a22aecb9da18d9d1de2daf828b4a1dbd9409
Ulyana <u.katrichko@yandex.ru>
2019-04-12 03:20:32 +0300

    codacy

---
+++ 4week/2task/Task2.Tests/ListTests.cs
@@ -0,0 +1,242 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Task2.Tests
+{
+    [TestClass]
+    public class ListTests
+    {
+        private List<int> emptyList;
+        private List<int> list;
+
+        [TestInitialize]
+        public void Initialization()
+        {
+            emptyList = new List<int>();
+            list = new List<int>();
+
+            list.AddNode(105, 1);
+            list.AddNode(267, 2);
+            list.AddNode(657, 3);
+            list.AddNode(987, 4);
+            list.AddNode(453, 3);
+            list.AddNode(65, 1);
+            list.AddNode(1004, 7);
+            list.AddNode(543, 5);
+        }
+
+        [TestMethod]
+        public void IsEmptyOnEmptyList()
+        {
+            Assert.IsTrue(emptyList.IsEmpty() && emptyList.Length == 0);
+        }
+
+        [TestMethod]
+        public void IsEmptyAfterAddAndDelete()
+        {
+            for (int i = 0; i < 100; ++i)
+            {
+                emptyList.AddNode(i, 1);
+            }
+            for (int i = 0; i < 100; ++i)
+            {
+                emptyList.DeleteNodeByPosition(1);
+            }
+            Assert.IsFalse(emptyList.IsEmpty() && emptyList.Length == 1);
+        }
+
+        [TestMethod]
+        public void CorrectAdd()
+        {
+            Assert.AreEqual("65 105 267 453 543 657 987 1004 ", list.GetStringOfListElements());
+        }
+
+        [TestMethod]
+        public void CorrectDeleteTest1()
+        {
+            list.DeleteNodeByPosition(1);
+            Assert.AreEqual("105 267 453 543 657 987 1004 ", list.GetStringOfListElements());
+        }
+
+        [TestMethod]
+        public void CorrectDeleteTest2()
+        {
+            list.DeleteNodeByPosition(1);
+            list.DeleteNodeByPosition(7);
+            Assert.AreEqual("105 267 453 543 657 987 ", list.GetStringOfListElements());
+        }
+
+        [TestMethod]
+        public void CorrectDeleteTest3()
+        {
+            list.DeleteNodeByPosition(1);
+            list.DeleteNodeByPosition(7);
+            list.DeleteNodeByPosition(3);
+            Assert.AreEqual("105 267 543 657 987 ", list.GetStringOfListElements());
+        }
+
+        [TestMethod]
+        public void CorrectDeleteTest4()
+        {
+            var positions = new int[] { 1, 7, 3, 3 };
+            for(int i = 0; i < 4; ++i)
+            {
+                list.DeleteNodeByPosition(positions[i]);
+            }
+            Assert.AreEqual("105 267 657 987 ", list.GetStringOfListElements());
+        }
+
+        [TestMethod]
+        public void CorrectDeleteTest5()
+        {
+            var positions = new int[] { 1, 7, 3, 3, 4 };
+            for (int i = 0; i < 5; ++i)
+            {
+                list.DeleteNodeByPosition(positions[i]);
+            }
+            Assert.AreEqual("105 267 657 ", list.GetStringOfListElements());
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(IncorrectPositionException))]
+        public void IncorrectDelete()
+        {
+            var positions = new int[] { 1, 7, 3, 3, 4, 4 };
+            for (int i = 0; i < 6; ++i)
+            {
+                list.DeleteNodeByPosition(positions[i]);
+            }
+            Assert.AreEqual("105 267 657 ", list.GetStringOfListElements());
+        }
+
+        [TestMethod]
+        public void CorrectDeleteTest7()
+        {
+            var positions = new int[] { 1, 7, 3, 3, 4, 1 };
+            for (int i = 0; i < 6; ++i)
+            {
+                list.DeleteNodeByPosition(positions[i]);
+            }
+            Assert.AreEqual("267 657 ", list.GetStringOfListElements());
+        }
+
+        [TestMethod]
+        public void CorrectDeleteTest8()
+        {
+            var positions = new int[] { 1, 7, 3, 3, 4, 1, 2 };
+            for (int i = 0; i < 7; ++i)
+            {
+                list.DeleteNodeByPosition(positions[i]);
+            }
+            Assert.AreEqual("267 ", list.GetStringOfListElements());
+        }
+
+        [TestMethod]
+        public void CorrectDeleteTest9()
+        {
+            var positions = new int[] { 1, 7, 3, 3, 4, 1, 2, 1 };
+            for (int i = 0; i < 8; ++i)
+            {
+                list.DeleteNodeByPosition(positions[i]);
+            }
+            Assert.AreEqual("List is empty", list.GetStringOfListElements());
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(IncorrectPositionException))]
+        public void AddToIncorrectPositionMoreThanLengthPlusOne()
+        {
+            list.AddNode(4, 10);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(IncorrectPositionException))]
+        public void AddToIncorrectZeroPosition()
+        {
+            list.AddNode(4, 0);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(IncorrectPositionException))]
+        public void DeleteFromIncorrectPositionMoreThanLength()
+        {
+            emptyList.AddNode(1, 1);
+            emptyList.AddNode(2, 2);
+            emptyList.AddNode(3, 3);
+            emptyList.DeleteNodeByPosition(4);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(IncorrectPositionException))]
+        public void DeleteFromIncorrectZeroPosition()
+        {
+            list.DeleteNodeByPosition(0);
+        }
+
+        [TestMethod]
+        public void ClearFunctionTest()
+        {
+            list.Clear();
+            Assert.IsTrue(list.IsEmpty());
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(IncorrectPositionException))]
+        public void IncorrectSetToZeroPosition()
+        {
+            list.SetValue(7, 0);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(IncorrectPositionException))]
+        public void IncorrectSetToPositionMoreThanLength()
+        {
+            list.SetValue(8, 9);
+        }
+
+        [TestMethod]
+        public void SetValueTest()
+        {
+            list.SetValue(11, 1);
+            list.SetValue(12, 2);
+            list.SetValue(13, 3);
+            list.SetValue(16, 4);
+            list.SetValue(24, 5);
+            list.SetValue(45, 6);
+            list.SetValue(56, 7);
+            list.SetValue(72, 8);
+            Assert.AreEqual("11 12 13 16 24 45 56 72 ", list.GetStringOfListElements());
+        }
+
+        [TestMethod]
+        public void GetValueTest1()
+        {
+            Assert.AreEqual(453, list.GetValue(4));
+        }
+
+        [TestMethod]
+        public void GetValueTest2()
+        {
+            Assert.AreEqual(65, list.GetValue(1));
+        }
+
+        [TestMethod]
+        public void GetValueTest3()
+        {
+            Assert.AreEqual(1004, list.GetValue(8));
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(IncorrectPositionException))]
+        public void GetValueOfIncorrectZeroPosition()
+        {
+            list.GetValue(0);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(IncorrectPositionException))]
+        public void GetValueOfIncorrectPositionMoreThanLength()
+        {
+            list.GetValue(9);
+        }
+    }
+}
---
+++ 4week/2task/Task2.Tests/UniqueListTests.cs
@@ -0,0 +1,129 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Task2.Tests
+{
+    [TestClass]
+    public class UniqueListTests
+    {
+        private UniqueList<int> list;
+
+        [TestInitialize]
+        public void Initialization()
+        {
+            list = new UniqueList<int>();
+            list.AddNode(-3);
+            list.AddNode(-2);
+            list.AddNode(-1);
+            list.AddNode(0);
+            list.AddNode(1);
+            list.AddNode(2);
+            list.AddNode(3);
+        }
+
+        [DataRow(-5)]
+        [DataRow(7)]
+        [DataRow(4)]
+        [DataRow(-8)]
+        [TestMethod]
+        public void CorrectAdd(int value)
+        {
+            list.AddNode(value);
+            Assert.AreEqual(value, list.GetValue(1));
+        }
+
+        [TestMethod]
+        public void CorrectDeleteTest1()
+        {
+            list.DeleteNode(-3);
+            Assert.AreEqual("3 2 1 0 -1 -2 ", list.GetStringOfListElements());
+        }
+
+        [TestMethod]
+        public void CorrectDeleteTest2()
+        {
+            list.DeleteNode(1);
+            list.DeleteNode(3);
+            Assert.AreEqual("2 0 -1 -2 -3 ", list.GetStringOfListElements());
+        }
+
+        [TestMethod]
+        public void CorrectDeleteTest3()
+        {
+            list.DeleteNode(-2);
+            list.DeleteNode(0);
+            list.DeleteNode(-1);
+            Assert.AreEqual("3 2 1 -3 ", list.GetStringOfListElements());
+        }
+
+
+        [DataRow(4, 1)]
+        [DataRow(6, 2)]
+        [DataRow(9, 7)]
+        [DataRow(-5, 6)]
+        [DataRow(-44, 4)]
+        [TestMethod]
+        public void CorrectSet(int value, int position)
+        {
+            list.SetValue(value, position);
+            Assert.AreEqual(value, list.GetValue(position));
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(AddExistingNodeException))]
+        public void AddExistingNodeToBegin()
+        {
+            list.AddNode(3);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(AddExistingNodeException))]
+        public void AddExistingNodeToEnd()
+        {
+            list.AddNode(-3);
+        }
+
+        [DataRow(0)]
+        [DataRow(-2)]
+        [DataRow(2)]
+        [DataRow(1)]
+        [DataRow(-1)]
+        [TestMethod]
+        [ExpectedException(typeof(AddExistingNodeException))]
+        public void AddExistingNodeInTheMiddle(int value)
+        {
+            list.AddNode(value);
+        }
+
+        [DataRow(-8)]
+        [DataRow(4)]
+        [DataRow(-4)]
+        [DataRow(7)]
+        [TestMethod]
+        [ExpectedException(typeof(DeleteNonExistentNodeException))]
+        public void DeleteNonExistentNode(int value)
+        {
+            list.DeleteNode(value);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(DeleteNonExistentNodeException))]
+        public void DeleteFromEmptyList()
+        {
+            list.Clear();
+            list.DeleteNode(2);
+        }
+
+        [DataRow(3, 1)]
+        [DataRow(-1, 2)]
+        [DataRow(1, 7)]
+        [DataRow(0, 3)]
+        [DataRow(-3, 5)]
+        [DataRow(-2, 4)]
+        [TestMethod]
+        [ExpectedException(typeof(ImpossibleSetException))]
+        public void SetExistingValue(int value, int position)
+        {
+            list.SetValue(value, position);
+        }
+    }
+}
