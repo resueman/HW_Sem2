ef8171a8a748bc8481024e90735de67e584e2877
Ulyana <u.katrichko@yandex.ru>
2019-04-17 23:50:50 +0300

    ++comments, ++tests, rename(tests)

---
+++ 6week/1task/Task1.Tests/Functions.Tests.cs
@@ -0,0 +1,119 @@
+using NUnit.Framework;
+using System.Collections.Generic;
+using Task1;
+
+namespace Tests
+{
+    public class Tests
+    {
+        public bool IsOperator(char symbol)
+            => symbol == '+' || symbol == '-' || symbol == '*' || symbol == '/';
+
+        private List<char> listOfChars;
+        private List<int> listOfNumbers;
+        private List<string> Slogans;
+
+        [SetUp]
+        public void Initialization()
+        {
+            listOfNumbers = new List<int>
+            {
+                1,
+                3,
+                7,
+                -2,
+                0,
+                15,
+                1000000,
+                -43000000
+            };
+
+            listOfChars = new List<char>()
+            {
+                'a',
+                'b',
+                'c',
+                'f',
+                'm',
+                'k',
+                'n',
+                'o',
+                'y',
+                'z'
+            };
+
+            Slogans = new List<string>() { "Freedom", "Equality", "Brotherhood",
+            "Cuba - yes", "Yankee - no", "Proletarians of all countries", "Unite",
+            "Wir", "Sind", "Das", "Volk" };
+
+        }
+
+        private bool CheckEquality<T>(List<T> initialList, List<T> resultList)
+        {
+            for (int i = 0; i < initialList.Count; ++i)
+            {
+                if (!initialList[i].Equals(resultList[i]))
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        [Test]
+        public void MapForIntType()
+        {
+            var result = new List<int> { 2, 6, 14, -4, 0, 30, 2000000, -86000000 };
+            listOfNumbers = Functions.Map(listOfNumbers, x => x * 2);
+            Assert.IsTrue(CheckEquality(listOfNumbers, result));
+        }
+
+        [Test]
+        public void MapForStringType()
+        {
+            var result = new List<string> { "Freedom!", "Equality!", "Brotherhood!",
+            "Cuba - yes!", "Yankee - no!", "Proletarians of all countries!", "Unite!",
+            "Wir!", "Sind!", "Das!", "Volk!" };
+            Slogans = Functions.Map(Slogans, word => word + "!");
+            Assert.IsTrue(CheckEquality(Slogans, result));
+        }
+
+        [Test]
+        public void FilterForIntType()
+        {
+            var result = new List<int> {-2, 0, -43000000 };
+            listOfNumbers = Functions.Filter(listOfNumbers, x => x <= 0);
+            Assert.IsTrue(CheckEquality(listOfNumbers, result));
+        }
+
+        [Test]
+        public void FilterForCharType()
+        {
+            var result = new List<char> { 'a', 'b', 'c', 'f', 'm', 'k', 'n' };
+            listOfChars = Functions.Filter(listOfChars, x => x <= 'n');
+            Assert.IsTrue(CheckEquality(listOfChars, result));
+        }
+
+        [Test]
+        public void FilterForStringType()
+        {
+            var result = new List<string> { "Brotherhood" , "Yankee - no", "Proletarians of all countries" };
+            Slogans = Functions.Filter(Slogans, x => x.Length > 10);
+            Assert.IsTrue(CheckEquality(Slogans, result));
+        }
+
+        [Test]
+        public void FoldForInt()
+        {
+            var list = new List<int> { 2, 6, 8, 10 };
+            Assert.AreEqual(6720, Functions.Fold(list, 7, ((x, val) => x * val)));
+        }
+
+        [Test]
+        public void FoldForString()
+        {
+            var list = new List<string> { "a", "b", "c", "l", "q", "z" };
+            Assert.AreEqual("zqlcbap", Functions.Fold(list, "p", ((x, val) => x + val)));
+        }
+    }
+}
--- 6week/1task/Task1/Functions.cs
+++ 6week/1task/Task1/Functions.cs
@@ -3,8 +3,19 @@ using System.Collections.Generic;
 
 namespace Task1
 {
+    /// <summary>
+    /// Functions that accept list and function, which works with list element
+    /// </summary>
     public static class Functions
     {
+        /// <summary>
+        /// Returns list, obtained by applying the transferred function
+        /// to each element of the transferred list
+        /// </summary>
+        /// <typeparam name="T">Type of list elements</typeparam>
+        /// <param name="list">List</param>
+        /// <param name="function">Function converting list element</param>
+        /// <returns>Changed list</returns>
         public static List<T> Map<T>(List<T> list, Func<T, T> function)
         {
             var resultList = new List<T>();
@@ -15,6 +26,14 @@ namespace Task1
             return resultList;
         }
 
+        /// <summary>
+        /// Returns list consisting of those elements of the transferred list, 
+        /// for which the transferred function returned true.
+        /// </summary>
+        /// <typeparam name="T">Type of list elements</typeparam>
+        /// <param name="list">List</param>
+        /// <param name="function">Function which returns boolean value</param>
+        /// <returns>Changed list</returns>
         public static List<T> Filter<T>(List<T> list, Func<T, bool> function)
         {
             var newList = new List<T>();
@@ -28,6 +47,16 @@ namespace Task1
             return newList;
         }
 
+        /// <summary>
+        /// Returns the accumulated value, which is obtained 
+        /// after the entire passage of the list.
+        /// </summary>
+        /// <typeparam name="T">Type of list elements</typeparam>
+        /// <param name="list">List</param>
+        /// <param name="initialValue">Initial value</param>
+        /// <param name="function">Function, which returns new accumulated value
+        /// by current accumulated value and current element</param>
+        /// <returns>Accumulated value</returns>
         public static T Fold<T>(List<T> list, T initialValue, Func<T, T, T> function)
         {
             T resultOfAccumulation = initialValue;
