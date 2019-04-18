a11fbff0eead5adb70cd0197603b4192a8375786
Ulyana <u.katrichko@yandex.ru>
2019-04-19 01:16:08 +0300

    started

---
+++ 7week/Clock/Clock/Form1.Designer.cs
@@ -0,0 +1,80 @@
+﻿namespace Clock
+{
+    partial class ClockForm
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.TimeLabel = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // TimeLabel
+            // 
+            this.TimeLabel.AutoEllipsis = true;
+            this.TimeLabel.AutoSize = true;
+            this.TimeLabel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.TimeLabel.Font = new System.Drawing.Font("Verdana", 38F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.TimeLabel.Location = new System.Drawing.Point(0, 0);
+            this.TimeLabel.Name = "TimeLabel";
+            this.TimeLabel.Size = new System.Drawing.Size(265, 61);
+            this.TimeLabel.TabIndex = 0;
+            this.TimeLabel.Text = "00:00:00";
+            this.TimeLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.Timer1_Tick);
+            // 
+            // ClockForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.ClientSize = new System.Drawing.Size(264, 66);
+            this.Controls.Add(this.TimeLabel);
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(280, 105);
+            this.MinimizeBox = false;
+            this.MinimumSize = new System.Drawing.Size(280, 0);
+            this.Name = "ClockForm";
+            this.Text = "Clock";
+            this.TopMost = true;
+            this.Load += new System.EventHandler(this.ClockForm_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Timer timer1;
+        public System.Windows.Forms.Label TimeLabel;
+    }
+}
+
---
+++ 7week/Clock/Clock/Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Clock
+{
+    public partial class ClockForm : Form
+    {
+        public ClockForm()
+        {
+            InitializeComponent();
+        }
+
+        private void Timer1_Tick(object sender, EventArgs e)
+        {
+            TimeLabel.Text = DateTime.Now.ToLongTimeString();                
+        }
+
+        private void ClockForm_Load(object sender, EventArgs e)
+        {
+            timer1.Start();
+        }
+    }
+}
