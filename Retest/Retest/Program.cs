4317c3ae59d8a6aead36340e93e504e60ac1366f
Ulyana <u.katrichko@yandex.ru>
2019-04-26 15:11:29 +0300

    rename

--- Retest/Retest/Program.cs
+++ Retest/Retest/Program.cs
@@ -16,7 +16,7 @@ namespace Retest
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new window());
+            Application.Run(new Window());
         }
     }
 }
