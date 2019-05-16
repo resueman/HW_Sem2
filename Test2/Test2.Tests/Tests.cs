a1a7ce93400b1617e1eb215f05da3901f8adc238
Ulyana <u.katrichko@yandex.ru>
2019-05-16 14:02:26 +0300

    created projects

---
+++ Test2/Test2.Tests/Tests.cs
@@ -0,0 +1,19 @@
+using NUnit.Framework;
+
+namespace Test2
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            
+
+        }
+    }
+}
